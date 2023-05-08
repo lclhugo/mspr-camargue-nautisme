63e2ceca2506bef7beefadff53a052611d015fdd
HaniLeb <hani.lebguiret@epsi.fr>
2023-05-08 17:05:44 +0200

    Reservation CRUD

---
+++ src/Form/ReservationFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Equipment;
+use App\Entity\Reservation;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReservationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nameClient')
+            ->add('emailClient')
+            ->add('equipment', EntityType::class, [
+                "class" => Equipment::class,
+                "choice_label" => "category.name"
+            ])
+            ->add('dateLocation', DateType::class, [
+                'format' => 'dd MM yyyy',
+                'data' => new \DateTime()
+            ])
+            // ->add('createdAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reservation::class,
+        ]);
+    }
+}
